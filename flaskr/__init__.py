0b5b1cb4d4dfcdda92acccb0b9569255b4021dc8
DDhanushka <dimuthudhanushka8@gmail.com>
2022-10-26 16:24:07 +0530

    blog templates and views

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,11 +2,12 @@ import os
 
 from flask import Flask
 
-def create_app(test_config = None):
+
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
@@ -28,5 +29,9 @@ def create_app(test_config = None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
