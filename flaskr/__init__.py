2bf16a115cb2111188a7159047c1f9c8ea76361e
DDhanushka <dimuthudhanushka8@gmail.com>
2022-10-26 16:19:37 +0530

    Add css

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,7 +26,7 @@ def create_app(test_config = None):
     from . import db
     db.init_app(app)
 
-    from . imort auth
+    from . import auth
     app.register_blueprint(auth.bp)
 
     return app
